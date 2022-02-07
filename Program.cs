f3310bbbd0672af6d08108fcb7983341ce23d723
Opilane TTHK <opilane@tthk.ee>
2022-02-07 13:32:23 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cstart
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a_text = Console.ReadLine();
+            double a;
+            a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine($"{a}");
+
+
+            string n1 = Console.ReadLine();
+            string n2 = Console.ReadLine();
+            if ((n1 == "Mihh" && n2 == "Marco") || (n1 == "Marco" && n2 == "Mihh") || (n1 == "Edgar" && n2 == "Marco") || (n1 == "Marco" && n2 == "Edgar")|| (n1 == "Aleksandr" && n2 == "Sasha") || (n1 == "Sasha" && n2 == "Aleksandr") || (n1 == "Sasha" && n2 == "Danil")|| (n1 == "Danil" && n2 == "Sasha") || (n1 == "Alina" && n2 == "Elena") || (n1 == "Elena" && n2 == "Alina")|| (n1 == "Ervin" && n2 == "Vlad") || (n1 == "Vlad" && n2 == "Ervin"))
+            {
+                Console.WriteLine("They are deskmates");
+            }
+            else
+            {
+                Console.WriteLine("They are not deskmates");
+            }
+            
+
+            //double price = 15;
+            //string t_type = "adult ticket";
+            ////Encoding enc = Encoding.UTF8;
+            //Console.WriteLine("Hello! What is your name?");
+            //Console.Title="Title";
+            //string name = Console.ReadLine();
+            //char [] letters=name.ToCharArray();//juku->["j","u","k","u"]
+            //int i = 0;
+            //name = "";
+            //foreach (var t in letters)
+            //{
+            //    if (i==0)
+            //    {
+            //        name += t.ToString().ToUpper();
+            //    }
+            //    else
+            //    {
+            //        name += t.ToString().ToLower();
+            //    }
+            //    i++;
+            //}
+
+            //Console.WriteLine("Hello {0}",name);
+            //if (name.ToUpper()=="JUKU" || name.ToLower()=="mark")
+            //{
+            //    Console.WriteLine($"Come visit! {name} Do you want to go to the cinema?");
+            //    string answer = Console.ReadLine();
+            //    if (answer.ToLower()=="yes" || answer.ToUpper()=="да")
+            //    {
+            //        int age = int.Parse(Console.ReadLine());
+            //        if (age <= 0 || age >= 109)
+            //        {
+            //            Console.WriteLine("rave");
+            //        }
+            //        else if (age >= 0 && age <= 6)
+            //        {
+            //            price -= price;
+            //            t_type = "Free ticket";
+            //        }
+            //        else if (age >= 6 && age <=14)
+            //        {
+            //            price -= price * 0.5;
+            //            t_type = "child ticket";
+            //        }
+            //        else if (age >=12 && age <=18)
+            //        {
+            //            price -= price * 0.3;
+            //            t_type = "student ticket";
+            //        }
+            //        else if (age >=18 && age <=65)
+            //        {
+            //            t_type = "adult ticket";
+            //        }
+            //        Console.WriteLine("{0},your {2} costs {1}", name, price,t_type);
+            //    }
+            //    else 
+            //    {
+            //        Console.WriteLine("Do not want? :(");
+            //    }
+            //}
+            //else 
+            //{
+            //    Console.WriteLine("I'm busy today! :(");
+
+            //}
+            //ConsoleKeyInfo klik;
+            //do
+            //{
+            //    klik = Console.ReadKey(true);
+            //    Console.Beep()
+            //} while (klik.Key != ConsoleKey.Escape);
+            //Environment.Exit();
+            Console.ReadKey();
+        }
+    }
+}
